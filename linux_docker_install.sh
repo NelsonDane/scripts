23260bdc4e75023c4d78de27a832b35722c6333f
Nelson Dane <danenb@clarkson.edu>
2022-11-07 21:22:47 -0500

    Don't prompt for input

--- linux_docker_install.sh
+++ linux_docker_install.sh
@@ -3,8 +3,8 @@
 # Script to install Docker on Linux
 
 # Set up the repository
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get update -y
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -18,8 +18,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker Engine
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # Add the current user to the docker group
 sudo usermod -aG docker $USER
